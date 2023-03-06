099cd2b87c9304d08d3e962c942ed3ce17160e41
Jesse Isooba <jesse4isooba@gmail.com>
2023-03-06 15:44:31 +0300

    My first task

---
+++ info.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * program that prints name, dob, and mobile number
+ *
+ * return 0;
+ *
+ */
+
+int main()
+{
+	char Name[] = "jESSE isooba";
+	char DOB [] = "october 4th 2001";
+	char phoneNumber[] = "0761686985";
+
+	printf("your contact details;\n");
+	printf("%s\n", Name);
+	printf("%s\n", DOB);
+	printf("%s\n", phoneNumber);
+
+	return 0;
+}
